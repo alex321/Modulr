29fd0527f6588af4c9fe3a7cdbf39d69d9e13948
alex321 <alexp.opensource@yahoo.co.uk>
2017-02-17 11:56:07 +0000

    Wire up to accounts service.

--- src/app/shared/accounts.service.ts
+++ src/app/shared/accounts.service.ts
@@ -1,8 +1,14 @@
 import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
 
 @Injectable()
 export class AccountsService {
-  getAccounts(page: number) { return []; }
+  constructor(private http: Http) {}
+
+  getAccounts(page: number) {
+    return this.http.get('./mock-data/results.json')
+        .map(resp => resp.json().content);
+  }
 
   getFilteredAccounts(page: number, search: string) { return []; }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,13 @@
 import {NgModule} from '@angular/core';
+
+import {AccountsService} from './accounts.service';
 import {SortCodePipe} from './sort-code.pipe';
 
 
-@NgModule({declarations: [SortCodePipe], exports: [SortCodePipe]})
+@NgModule({
+  declarations: [SortCodePipe],
+  providers: [AccountsService],
+  exports: [SortCodePipe]
+})
 export class SharedModule {
 }
